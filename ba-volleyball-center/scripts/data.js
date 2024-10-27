eaf0215095b90ecec792f35d976e511e7611cab6
santichapa <santi.chaparro280@gmail.com>
2024-10-26 22:14:19 -0300

    added api data

---
+++ ba-volleyball-center/scripts/data.js
@@ -0,0 +1,14 @@
+// var myHeaders = new Headers();
+// myHeaders.append("x-rapidapi-key", "1c8d061eb66ee9c506c3df6c6cc0eb11");
+// myHeaders.append("x-rapidapi-host", "v1.volleyball.api-sports.io");
+
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+//   redirect: 'follow'
+// };
+
+// fetch("https://v1.volleyball.api-sports.io/leagues", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
